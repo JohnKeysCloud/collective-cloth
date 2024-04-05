db14a049aaa0bae540932f6412313c50fdc044de
JOHN KEYS CLOUD <90482169+JohnKeysCloud@users.noreply.github.com>
2024-04-05 17:19:12 -0400

    move api repo inside dist (root of project)

---
+++ api/submit-form.js
@@ -0,0 +1,26 @@
+export default async (req, res) => {
+  if (req.method === 'POST') {
+    const {
+      NAME,
+      EMAIL,
+      PHONE,
+      COLLECTIVE_TYPE,
+      GARMENT_TYPE,
+      COLOR,
+      QUANTITY,
+      VISION
+    } = req.body; // Example fields from your form
+
+    console.log(NAME, EMAIL, PHONE, COLLECTIVE_TYPE, GARMENT_TYPE, COLOR, QUANTITY, VISION);
+    // Handle your form submission logic here
+    // For example: 
+    // Send an email(SendGrid, AWS SES, or NodeMailer with an SMTP server)
+    // Save to a database… etc.
+
+    return res.status(200).json({ status: 'Form submitted successfully!' });
+  }
+
+  // Handle any other HTTP methods
+  res.setHeader('Allow', ['POST']);
+  res.status(405).end(`Method ${req.method} Not Allowed`);
+};
