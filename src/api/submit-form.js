b39763c53a9ee05fb86b4744de6c59dc34d6af92
JOHN KEYS CLOUD <90482169+JohnKeysCloud@users.noreply.github.com>
2024-04-08 05:42:36 -0400

    adjust send email function arguments

--- src/api/submit-form.js
+++ src/api/submit-form.js
@@ -25,7 +25,7 @@ export default async (req, res) => {
 
       const emailMarkup = createEmailMarkup(req.body);
       
-      await sendEmailWithHtmlFile('test@example.com', 'your-email@example.com', 'Sending with SendGrid is Fun', emailMarkup);
+      await sendEmailWithHtmlFile('johncloud@cyclonestud.io', 'admin@collectivecloth.com', 'New Customer Request Zaddy 🤪', emailMarkup);
 
       res.status(200).json({ status: 'success', message: 'Data received and processed' });
     } catch (error) {
